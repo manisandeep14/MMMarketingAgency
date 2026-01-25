cafae6b1c9c7307a6623d97e807904283fb07ecd
manisandeep14 <manisandeepms32m16@gmail.com>
2026-01-25 13:30:33 +0530

    modifications

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDatabase = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MMMarketing-Database', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MMMarketingAgency', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
