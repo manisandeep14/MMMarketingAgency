b4a09f5927c0b55196d925646abb4845fe5ed6a6
manisandeep14 <manisandeepms32m16@gmail.com>
2026-01-26 09:17:06 +0530

    final checkup

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,29 +1,4 @@
 //vercel files
-
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-import authRoutes from "./auth/index.js";
-
-dotenv.config();
-
-const app = express();
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    process.env.FRONTEND_URL,
-  ],
-  credentials: true,
-}));
-
-app.use(express.json());
-
-app.use("/auth", authRoutes);
-
-app.get("/", (req, res) => {
-  res.json({ success: true, message: "Backend running on Vercel 🚀" });
-});
+import app from "../src/server.js";
 
 export default app;
