1eecb4c8c45600e2aa2260fa13c900f176b0dd7d
manisandeep14 <manisandeepms32m16@gmail.com>
2026-02-03 18:34:23 +0530

    render fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -84,10 +84,9 @@ app.use((err, req, res, next) => {
 /* 🔹 LOCAL SERVER ONLY (NOT VERCEL) */
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 export default app;
