2dced6f7dc6464723c44f7d2d29102cfe1fff4ce
manisandeep14 <manisandeepms32m16@gmail.com>
2026-02-03 18:17:04 +0530

    render fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -85,7 +85,7 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
