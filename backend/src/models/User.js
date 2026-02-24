db8982783864150a297ea8ac04ee4ef6631af567
manisandeep14 <manisandeepms32m16@gmail.com>
2026-02-24 18:32:41 +0530

    BuggFix

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcryptjs';
 /* ---------------- ADDRESS SCHEMA ---------------- */
 const addressSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
+  phone: { type: String, required: true }, 
   addressLine1: { type: String, required: true },
   addressLine2: String,
   city: { type: String, required: true },
