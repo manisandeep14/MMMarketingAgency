ac241be8bf6918cef71051725368e0bcba434083
manisandeep14 <manisandeepms32m16@gmail.com>
2026-03-08 22:03:21 +0530

    manual bug

--- backend/src/utils/geocodeAddress.js
+++ backend/src/utils/geocodeAddress.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 export const geocodeAddress = async (address) => {
+
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.GOOGLE_MAPS_KEY}`;
 
   const res = await axios.get(url);
 
-  // Safety check
+  // VERY IMPORTANT CHECK
   if (!res.data.results || res.data.results.length === 0) {
-    throw new Error("Unable to find location for the given address");
+    throw new Error("Google could not find the address location");
   }
 
   const location = res.data.results[0].geometry.location;
